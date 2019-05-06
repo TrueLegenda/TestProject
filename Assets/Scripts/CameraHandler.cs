770ba96e5b5c719ebdf61f1dfcb3c9f5b5c0c782
BuildTools <unconfigured@null.spigotmc.org>
2019-05-06 23:30:14 +0300

    first commit

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject sphere;
+
+    void Update()
+    {
+        Vector3 pos = sphere.transform.position;
+        Vector3 newPos = new Vector3(pos[0] + 4, pos[1] + 3, pos[2]);
+        camera.transform.position = newPos;
+    }
+}
---
+++ Assets/Scripts/SphereHandler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereHandler : MonoBehaviour
+{
+    private bool collisionDetected = true;
+    public GameObject sphere;
+    public Rigidbody rb;
+    public float v;
+    public float vCap;
+    public float thrust;
+
+    void Start()
+    {
+        Time.fixedDeltaTime = Time.deltaTime;
+        v = 1f;
+        vCap = 1.5f;
+        thrust = 0.23f;
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.AddForce(-thrust * v, 0f, 0f, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.AddForce(thrust * v, 0f, 0f, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(0f, 0f, thrust * v, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(0f, 0f, -thrust * v, ForceMode.VelocityChange);
+            }
+
+            if(Input.GetKey(KeyCode.Space) && collisionDetected)
+            {
+                print(sphere.transform.position);
+                rb.AddForce(0f, 2f, 0f, ForceMode.Impulse);
+                print(sphere.transform.position);
+            }
+
+            if (v <= vCap)
+            {
+                v += 0.05f;
+            }
+
+        }
+        else
+        {
+            v = 1;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collisionDetected = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collisionDetected = false;
+    }
+
+}
