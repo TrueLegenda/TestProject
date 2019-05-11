18ad27174b6837ffa61a6372cb296db247e67f5a
BuildTools <unconfigured@null.spigotmc.org>
2019-05-11 04:04:33 +0300

    MouseLock On Center

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CameraHandler : MonoBehaviour
 {
     public GameObject cam;
     public float yDeg = 30;
     public float xDeg = 90;
+    public int sensetivity = 3;
     public float r = 7f;
     public GameObject sphere;
     private Vector3[] positions = new Vector3[2];
@@ -15,36 +17,34 @@ public class CameraHandler : MonoBehaviour
     void Start()
     {
         Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Confined;
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
 
     void Update()
     {
-        print(cam);
         // Variables
-        positions[0] = positions[1];
-        positions[1] = Input.mousePosition;
-        float xOffset = positions[1].x - positions[0].x;
-        float yOffset = positions[1].y - positions[0].y;
+        float xOffset = Input.GetAxis("Mouse X");
+        float yOffset = Input.GetAxis("Mouse Y");
         float x = r * Mathf.Sin(DegToRadians(xDeg)) * Mathf.Cos(DegToRadians(yDeg));
         float y = r * Mathf.Sin(DegToRadians(yDeg)) * Mathf.Sin(DegToRadians(xDeg));
         float z = r * Mathf.Cos(DegToRadians(xDeg));
 
         // Camera Controller
-        cam.transform.position = new Vector3(x, y, z) + sphere.transform.position;
-        xDeg += xOffset / 10;
-        yDeg -= yOffset / 10;
-
-        if(Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            print(Input.mousePosition);
-        }
-
+        Vector3 pos = new Vector3(x, y, z) + sphere.transform.position;
+        cam.transform.position = pos;
+        xDeg = MinMax(30, 150, xDeg + (xOffset / sensetivity));
+        yDeg = MinMax(-15, 35, yDeg - (yOffset / sensetivity));
+        
         this.transform.LookAt(sphere.transform);
+    }
 
-        // Zoom
-        print(Input.mouseScrollDelta);
+    float MinMax(float min, float max, float value)
+    {
+        value = Math.Max(min, value);
+        value = Math.Min(max, value);
+
+        return value;
     }
 
     float DegToRadians(float deg)
